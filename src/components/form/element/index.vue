80f6ccd7233b1a0bd377352feb6dccbf33b31a57
小楠瓜 <happydemoney@126.com>
2020-06-06 18:21:33 +0800

    Update index.vue

--- src/components/form/element/index.vue
+++ src/components/form/element/index.vue
@@ -1,51 +1,55 @@
 <template>
-    <component
-        :is="componentType"
-        :renderConfig="renderConfig"
-        :value="value"
-        @input="handleInput"/>
+  <component
+    :is="componentType"
+    :renderConfig="renderConfig"
+    :value="value"
+    @input="handleInput"
+  />
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Input from './Input.vue'
-import Custom from './Custom.vue'
+import Vue from "vue";
+import Custom from "./Custom.vue";
+import { RenderConfig } from "../Form.vue";
 
 const ComponentsMap = {
-    Input,
-    custom: Custom
-}
+  custom: Custom
+};
 
 export default Vue.extend({
-    components: ComponentsMap,
-    props: {
-        value: {
-            default: null
-        },
-        renderConfig: {
-            type: Object,
-            default: null
-        }
+  components: ComponentsMap,
+  props: {
+    value: {
+      default: null
     },
-    computed: {
-        componentType () {
-            return this.renderConfig.type
-        }
-    },
-    data () {
+    renderConfig: {
+      type: Object,
+      default: function() {
         return {
-            val: this.value
-        }
-    },
-    watch: {
-        value (newVal, oldVal) {
-            this.val = newVal
-        }
-    },
-    methods: {
-        handleInput (val: any) {
-            this.$emit('input', val)
-        }
+          type: "input"
+        };
+      }
     }
-})
+  },
+  computed: {
+    componentType() {
+      return (this.renderConfig as RenderConfig).type;
+    }
+  },
+  data() {
+    return {
+      val: this.value
+    };
+  },
+  watch: {
+    value(newVal, oldVal) {
+      this.val = newVal;
+    }
+  },
+  methods: {
+    handleInput(val: any) {
+      this.$emit("input", val);
+    }
+  }
+});
 </script>
