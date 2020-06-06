d994a93e20c0dd924a5ae99f8e79ddac1b8d01a4
小楠瓜 <happydemoney@126.com>
2020-06-06 18:14:16 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  publicPath: '/vue-admin-configure/'
+  // chainWebpack: config => {
+  //   config.plugin('html').tap(args => {
+  //       args[0].minify = {
+  //           removeAttributeQuotes: false
+  //       }
+  //       return args
+  //   })
+  // }
+}
