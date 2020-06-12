fe30099456623b3ebb97609371642f45e97dccbf
小楠瓜 <happydemoney@126.com>
2020-06-12 20:24:56 +0800

    Create algorithm.ts

---
+++ src/router/modules/algorithm.ts
@@ -0,0 +1,23 @@
+import Layout from "@/layout/index.vue";
+
+const AlgorithmRouter = {
+  path: "/algorithm",
+  component: Layout,
+  redirect: "noRedirect",
+  name: "Sort",
+  meta: {
+    title: "Sort",
+    icon: "s-order"
+  },
+  children: [
+    {
+      path: "sort",
+      component: () =>
+        import(/* webpackChunkName: "line" */ "@/views/algorithm/sort.vue"),
+      name: "algorithmSort",
+      meta: { title: "algorithm sort", icon: "s-goods", noCache: false }
+    }
+  ]
+};
+
+export default AlgorithmRouter;
