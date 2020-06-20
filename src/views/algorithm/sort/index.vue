d02e60138422e9da2eb645e8b14cb93e980b4514
小楠瓜 <happydemoney@126.com>
2020-06-20 15:24:56 +0800

    Create index.vue

---
+++ src/views/algorithm/sort/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrap">
+    <bubble-sort></bubble-sort>
+    <select-sort></select-sort>
+    <quick-sort></quick-sort>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "algorithmSort",
+  components: {
+    BubbleSort: () => import("./bubble.vue"),
+    SelectSort: () => import("./select.vue"),
+    QuickSort: () => import("./quick.vue")
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.wrap {
+  width: 100%;
+}
+</style>
